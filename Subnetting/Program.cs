088b53dec14df2b2bd7c769c41874ec82de60d0b
Florian Paintinger <fpaintinger@web.de>
2024-05-01 12:00:32 +0200

    Aufgabenstellung des Programms erstellt

--- Subnetting/Program.cs
+++ Subnetting/Program.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Subnetting
-{
+{   // Erstellt werden soll ein Programm, bei dem der Benutzer eine IP-Adresse und eine Subnetzmaske angezeigt bekommt. 
+    // Anschließend soll der Benutzer die Netzadresse, die erste IP-Adresse, die letzte IP-Adresse und die Broadcastadresse eingeben.
+    // Das Programm überprüft, ob die eingegebenen Adressen korrekt sind.
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
+        
         }
     }
 }
