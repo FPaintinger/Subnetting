294302edde71eba98bc9777e77ab0b277e010681
Florian Paintinger <fpaintinger@web.de>
2024-05-01 12:20:46 +0200

    Funktion "Create Random IP" erstellt.

--- Subnetting/Program.cs
+++ Subnetting/Program.cs
@@ -11,8 +11,34 @@ namespace Subnetting
     internal class Program
     {
         static void Main(string[] args)
-        {   
-        
+        {
+            CreateRandomIP();
+            
+        }
+
+        // Funktion: erstellen einer zufälligen, gültigen IP-Adresse
+        // 
+        static String CreateRandomIP()
+        {
+            String return_IP;
+            int num;
+            Random random = new Random();            
+            
+            num = random.Next(2, 255 + 1);          // Erstelle eine beliebige Nummer zw. 2 und 255 für das 1. Oktett
+            String zahl1 = Convert.ToString(num);   // wandle den Datentyp int in String um
+            return_IP = zahl1;                      // weise return_IP den Wert von zahl1 zu
+            
+            for (int i = 3;i != 0; i--)             // Erstelle eine beliebige Nummer zw. 0 und 255 für das 2., 3. und 4. Oktett
+            {
+                num = random.Next(0, 255 + 1);
+                zahl1 = Convert.ToString(num);
+                return_IP = return_IP + "." + zahl1;
+            }
+
+            Console.WriteLine(return_IP);
+
+
+            return return_IP;
         }
     }
 }
